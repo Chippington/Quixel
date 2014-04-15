e64e79038f288459d99c8fd568fcaa416a3059b0
Chippington <scythix@hotmail.com>
2014-04-14 19:13:17 -0700

    Updated for Release

--- ITerrainGenerator.cs
+++ ITerrainGenerator.cs
@@ -53,28 +53,7 @@ namespace Quixel
             yy = pos.y;
             zz = pos.z;
 
-            float d = yy - 50f;
-
-            float warp = SimplexNoise.Noise.Generate(xx / 100f, yy / 100f, zz / 100f);
-            xx += warp;
-            yy += warp;
-            zz += warp;
-
-            float noise = SimplexNoise.Noise.Generate(xx / 500f, 0f, zz / 500f);
-            d += noise * 70.5f;
-
-            noise = SimplexNoise.Noise.Generate(xx / 100f, 0f, zz / 100f);
-            d += noise * SimplexNoise.Noise.Generate(xx / 200f, 0f, zz / 200f) * 10f;
-
-            noise = SimplexNoise.Noise.Generate(xx / 4000f, 0f, zz / 4000f);
-            d += noise * 300f;
-
-            noise = SimplexNoise.Noise.Generate(xx / 10000f, 0f, zz / 10000f);
-            d += noise * 1400f;
-
-            if (yy < -150f)
-                d -= (SimplexNoise.Noise.Generate(xx / 680f, yy / 600f, zz / 680f) * 190f) * ((yy + 150f) / 300f);
-
+            float d = yy - (-50f);
 
             return new VoxelData()
             {
